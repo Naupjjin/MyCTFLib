75884a227de7ad14549d0bafbf0f1789e8a7638e
Naup <naup96721@gmail.com>
2024-12-17 18:59:49 +0800

    Windows Kernel exploitation

---
+++ WindowsKernelPwnLib/libs/VAtoPA.py
@@ -0,0 +1,14 @@
+def PAtoVA_4page_cal(address):
+    PML4_offset = (address >> 39) & 0b111111111
+    PDPT_offset = (address >> 30) & 0b111111111
+    PD_offset = (address >> 21) & 0b111111111
+    PT_offset = (address >> 12) & 0b111111111
+    PhysicalAddress_offset = (address) & 0b111111111
+
+    print("PML4_offset: ",hex(PML4_offset))
+    print("PDPT_offset: ",hex(PDPT_offset))
+    print("PD_offset: ",hex(PD_offset))
+    print("PT_offset: ",hex(PT_offset))
+    print("PhysicalAddress_offset: ",hex(PhysicalAddress_offset))
+
+
